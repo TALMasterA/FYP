d6f2545279afebe1ab475e28bd1db6f8a5278790
TALMasterA <22235876@life.hkbu.edu.hk>
2026-01-14 23:55:05 +0800

    Upload tree map of important files for the app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.fyp"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "AZURE_SPEECH_KEY", "\"\"")
